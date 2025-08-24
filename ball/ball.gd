afaad31bf9cc04094dfb1bdac48ff8b20f30252f
robert.brosh23 <robert.brosh23@gmail.com>
2025-08-24 10:45:17 -0400

    fixed bumper physics

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,13 +2,11 @@ class_name Ball extends RigidBody2D
 
 @onready var collisionShape = $CollisionShape2D
 
+func _ready():
+	linear_velocity = Vector2(0, -300)
+
 func _physics_process(delta: float) -> void:
 	pass
-
-
-func _on_body_entered(body: Node) -> void:
-	if body is Flipper:
-		pass
 			
 func _on_body_exited(body: Node) -> void:
 	if body is Flipper:
--- bumper/bumper.gd
+++ bumper/bumper.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name Bumper extends StaticBody2D
 
 const BUMPER_STRENGTH = 20
 
@@ -7,15 +7,24 @@ const BUMPER_STRENGTH = 20
 var laser_1_wav = preload("res://assets/sounds/Laser_1.wav")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	
 	if body is Ball:
+		print("ball position: ", body.position, "bumper position: ", position)
 		var normal = (body.position - position).normalized()
+		if body.linear_velocity.dot(normal) > 0:
+			normal = -normal
 		var angle = normal.angle()
-		var reflected = body.linear_velocity - 2.0 * body.linear_velocity.dot(normal) * normal
+		print(angle)
+		
+		var reflected = body.linear_velocity.bounce(normal)
 		body.linear_velocity = reflected
 		
-		# body.apply_central_impulse(Vector2(cos(angle) * BUMPER_STRENGTH, sin(angle) * BUMPER_STRENGTH))
+		#body.apply_central_impulse(Vector2(cos(angle) * BUMPER_STRENGTH, sin(angle) * BUMPER_STRENGTH))
 		
 		animation_player.play("bumber_hit")
+		
+func bump(ball: Ball):
+	animation_player.play("bumber_hit")
 
 func play_sound(sound: Resource) -> void:
 	var audioPlayer = AudioStreamPlayer.new()
